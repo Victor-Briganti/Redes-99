893c99dd69912142e1aed7fac1ad1c76c9c06dca
João Victor Briganti <jvbriganti@gmail.com>
2026-06-30 09:56:30 -0300

    Adicionado principio da automatização

--- run.sh
+++ run.sh
@@ -4,3 +4,15 @@
 
 # podman run --rm --name pg-test -p 5432:5432 --network my-network test
 # podman run -it --rm --name node-test -p 3000:3000 --network my-network test2
+
+# ssh-keygen -t ed25519 -f $(pwd)/ssh/key -N ""
+
+# butane --pretty --strict config.yml > config.ign
+
+VM="MinhaVM"
+OVA="/home/nessuno/Downloads/fedora-coreos.ova"
+IGNITION="pointer.ign"
+VBoxManage import "$OVA" --vsys 0 --vmname "$VM"
+VBoxManage modifyvm "$VM" --nic2 hostonly --hostonlyadapter2 vboxnet0
+VBoxManage guestproperty set "$VM" "/Ignition/Config" "$(cat "$IGNITION")"
+VBoxManage startvm "$VM"
