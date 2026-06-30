2c796c7551dcf5fe6e60a38b4aae3d13ce67385d
João Victor Briganti <jvbriganti@gmail.com>
2026-06-30 17:17:19 -0300

    Ajustado problemas na config

--- run.sh
+++ run.sh
@@ -4,28 +4,12 @@ VM="VM"
 OVA="/home/nessuno/Downloads/fedora-coreos.ova"
 IGNITION="pointer.ign"
 
-rm -f app.tar.gz
-tar -czf app.tar.gz db web
+butane --pretty --strict pointer.yml >pointer.ign
+butane -d . --pretty --strict config.yml >config.ign
 
-# rm -f ssh/key ssh/key.pub
-# ssh-keygen -t ed25519 -f $(pwd)/ssh/key -N ""
-
-# SSH_KEY="$(cat ssh/key.pub)"
-# while IFS= read -r line; do
-#   printf '%s\n' ${line//\$\{SSH_KEY\}/$SSH_KEY}
-# done <pointer.yml | butane --pretty --strict >pointer.ign
-
-# butane --pretty --strict pointer.yml >pointer.ign
-# butane --pretty --strict config.yml >config.ign
-
-# python -m http.server 8080 &
-
-# if VBoxManage list runningvms | grep -q "\"$VM\""; then
-#   VBoxManage controlvm "$VM" poweroff
-#   sleep 10
-# fi
-#
-# VBoxManage unregistervm "$VM" --delete
+if ! ss -ltnp | grep 8080 &>/dev/null; then
+  python -m http.server 8080 &
+fi
 
 VBoxManage import "$OVA" --vsys 0 --vmname "$VM"
 VBoxManage modifyvm "$VM" --nic2 hostonly --hostonlyadapter2 vboxnet0
